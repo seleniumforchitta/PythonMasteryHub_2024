ca919051092b989a6596a62158bbeb57b5baa0f8
seleniumforchitta <seleniumforchitta@gmail.com>
2023-12-29 12:18:34 +0530

    adding pythonFlowControlPractice.py

---
+++ PythonProgramizRevision_Jan2024/pythonFlowControlPractice.py
@@ -0,0 +1,33 @@
+number = 10
+
+# check if number is greater than 0
+if number > 0:
+    print('Number is positive.')
+
+print('The if statement is easy')
+
+# ---------------------------------------------------------------
+
+number = 10
+
+if number > 0:
+    print('Positive number')
+
+else:
+    print('Negative number')
+
+print('This statement is always executed')
+
+# ---------------------------------------------------------------
+
+number = 0
+
+if number > 0:
+    print("Positive number")
+
+elif number == 0:
+    print('Zero')
+else:
+    print('Negative number')
+
+print('This statement is always executed')
