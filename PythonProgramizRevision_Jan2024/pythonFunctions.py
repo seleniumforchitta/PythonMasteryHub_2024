7567a30a2ec8be5d15a155489dec9396402f253a
seleniumforchitta <seleniumforchitta@gmail.com>
2024-02-25 02:41:10 +0530

    adding pythonFlowControlPractice.py

---
+++ PythonProgramizRevision_Jan2024/pythonFunctions.py
@@ -0,0 +1,28 @@
+def function_name():
+    print("Hello")
+
+function_name()
+def greet(name):
+    print("Hello", name);
+greet("Jake");
+
+
+def sumOfAllListMembers(list):
+    sum = 0;
+    for i in list:
+        sum = sum + i;
+    return sum;
+
+def squaredNumFromList(list):
+    list1 = [];
+    for i in list:
+        list1.append(i*i);
+    return list1;
+
+abc = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+print(sumOfAllListMembers(abc));
+print(squaredNumFromList(abc))
+
+
+
+
