8e5ab0958fc776b903acd0c417bb25f9a02b32d6
seleniumforchitta <seleniumforchitta@gmail.com>
2024-08-12 17:19:38 +0530

    slenium is added to the project.

---
+++ PythonSeleniumRevision_2024/myFirstSeleniumProgram.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Example usage
+driver.get("https://www.google.com")
