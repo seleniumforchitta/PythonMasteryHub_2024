c7a25a98156442af8e294a0cc4ceb2bd0cf662d0
seleniumforchitta <seleniumforchitta@gmail.com>
2024-09-08 09:15:25 +0530

    Formatting all file names

---
+++ PythonProgramizRevision_Jan2024/pyDictionaryPractice.py
@@ -0,0 +1,9 @@
+# creating a dictionary
+country_capitals = {
+    "Germany": "Berlin",
+    "Canada": "Ottawa",
+    "England": "London"
+}
+
+# printing the dictionary
+print(country_capitals)  # {'Germany': 'Berlin', 'Canada': 'Ottawa', 'England': 'London'}
