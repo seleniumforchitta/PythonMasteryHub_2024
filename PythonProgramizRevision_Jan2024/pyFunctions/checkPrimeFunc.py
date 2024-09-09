894cfbfbff212d6bbc86b34af7cc3a0c62b26e6f
seleniumforchitta <seleniumforchitta@gmail.com>
2024-09-09 23:57:54 +0530

    Implimenting check prime method

--- PythonProgramizRevision_Jan2024/pyFunctions/checkPrimeFunc.py
+++ PythonProgramizRevision_Jan2024/pyFunctions/checkPrimeFunc.py
@@ -1,9 +1,9 @@
 # Write a function to check if a given number is prime or not.
 def checkprime(num):
     if num <= 1:
-        print(num, " is a prime number !")
+        print(num, " is not a prime number !")
     else:
-        for i in range(2, int(num**0.5)+1):
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 print(num, " is not a prime number !")
                 return
@@ -12,6 +12,8 @@ def checkprime(num):
                 return
 
 
+checkprime(0)
+checkprime(1)
 checkprime(13)
 checkprime(4)
 checkprime(2)
@@ -20,3 +22,14 @@ checkprime(101)
 checkprime(111)
 checkprime(122)
 checkprime(123)
+
+# o/p -
+# 0  is not a prime number !
+# 1  is not a prime number !
+# 13  is a prime number !
+# 4  is not a prime number !
+# 21  is a prime number !
+# 101  is a prime number !
+# 111  is a prime number !
+# 122  is not a prime number !
+# 123  is a prime number !
