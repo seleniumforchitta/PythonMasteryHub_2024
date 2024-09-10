1bd54368197dd6f9f46b2219903a3736f082d597
seleniumforchitta <seleniumforchitta@gmail.com>
2024-09-10 23:21:36 +0530

    Read and Write CSV Files

--- PythonProgramizRevision_Jan2024/pyFiles/pyCSVReadAndWrite.py
+++ PythonProgramizRevision_Jan2024/pyFiles/pyCSVReadAndWrite.py
@@ -7,6 +7,23 @@ with open('C:\\Users\\chitt\\PycharmProjects\\PythonMasteryHub_2024\\PythonProgr
     for row in reader:
         print(row)
 
+#  Read CSV file Having Tab Delimiter
+with open('person.csv', 'r') as file:
+    reader = csv.reader(file, delimiter='\t')
+    for row in reader:
+        print(row)
+
+# Read CSV files with initial spaces
+with open('person.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, skipinitialspace=True)
+    for row in reader:
+        print(row)
+
+# Read CSV files with quotes
+with open('person.csv', 'r') as file:
+    reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    for row in reader:
+        print(row)
 file.close()
 
 with open('C:\\Users\\chitt\\PycharmProjects\\PythonMasteryHub_2024\\PythonProgramizRevision_Jan2024\\pyFiles'
@@ -27,3 +44,33 @@ with open('C:\\Users\\chitt\\PycharmProjects\\PythonMasteryHub_2024\\PythonProgr
         print(row)
 
 file.close()
+
+# Writing Multiple Rows with writerows()
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('protagonist.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)
+file.close()
+
+# Write CSV File Having Pipe Delimiter
+data_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='|')
+    writer.writerows(data_list)
+
+# Write CSV files with quotes
+row_list = [
+    ["SN", "Name", "Quotes"],
+    [1, "Buddha", "What we think we become"],
+    [2, "Mark Twain", "Never regret anything that made you smile"],
+    [3, "Oscar Wilde", "Be yourself everyone else is already taken"]
+]
+with open('quotes.csv', 'w', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+    writer.writerows(row_list)
---
+++ PythonProgramizRevision_Jan2024/pyFunctions/pyRecursion.py
@@ -0,0 +1,9 @@
+# Find the factorial of a number
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))  # 120
